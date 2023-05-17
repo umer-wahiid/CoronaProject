cb8fd1238273a4ec630346e63dec81f63671e89f
Umer Abdul Wahid <umerwahiid48@gmail.com>
2023-05-18 00:53:14 +0500

    Component View Done

---
+++ ECProject/ECProject/Models/CategoriesViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECProject.Models
+{
+    public class CategoriesViewComponent:ViewComponent
+    {
+        public ECDbContext _context;
+
+        public CategoriesViewComponent(ECDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+}
---
+++ ECProject/ECProject/Models/ProductsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECProject.Models
+{
+    public class ProductsViewComponent : ViewComponent
+    {
+        public ECDbContext _context;
+
+        public ProductsViewComponent(ECDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+    }
+}
